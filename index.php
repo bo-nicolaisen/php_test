3a47270f08acb97078da825ca1f1a7b1ec7d1579
bo-nicolaisen <bni@techcollege.dk>
2019-10-21 11:58:10 +0200

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-echo "Hello world!";
-?>
+ <!DOCTYPE html>
+ <html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <title>TITLE</title>
+     <meta name="description" content="DESCRIPTION">
+    <link rel="stylesheet" href="PATH">
+
+     <!--[if lt IE 9]>
+       <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+ </head>
+
+ <body>
+<h1>test page</h1>
+   <?php
+   echo "Hello world!";
+   ?>
+
+
+ </body>
+
+ </html>
